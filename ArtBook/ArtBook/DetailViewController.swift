1e9acea3ed241be3c5be85003dee11a6367c3550
Halil Ibrahim Andic <hiandic@gmail.com>
2023-02-11 20:49:42 +0300

    image picker added

--- ArtBook/ArtBook/DetailViewController.swift
+++ ArtBook/ArtBook/DetailViewController.swift
@@ -18,11 +18,38 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+    }
     
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true //editing
+        present(picker, animated: true, completion: nil)
+        
+        print("gorsele tikladim")
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
     }
     
     @IBAction func saveButton(_ sender: UIButton) {
     }
-    
 
+}
+
+//MARK: - Extensions
+extension DetailViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage  // I'm not 100% sure that user selected an image. so used as? casting
+        self.dismiss(animated: true, completion: nil)
+    }
 }
