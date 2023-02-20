ea8d35c809e76c1d37bc920550bfb04e79f37ec3
Halil Ibrahim Andic <hiandic@gmail.com>
2023-02-20 16:26:53 +0300

    /GuessTheFlag: created

---
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Halil Ibrahim Andic on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
