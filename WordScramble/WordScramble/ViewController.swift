b8430a8f58dd974bb98d8eccc432ca5ce835d732
Halil Ibrahim Andic <hiandic@gmail.com>
2023-02-22 16:03:53 +0300

    /WordScramble: created

---
+++ WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Halil Ibrahim Andic on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
